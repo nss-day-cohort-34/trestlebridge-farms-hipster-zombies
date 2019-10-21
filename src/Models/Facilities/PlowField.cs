4cfb93f0d91f458aceb3289fa75774dd0357ceae
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-21 09:32:11 -0500

    added Plow field

--- src/Models/Facilities/PlowField.cs
+++ src/Models/Facilities/PlowField.cs
@@ -5,12 +5,12 @@ using Trestlebridge.Interfaces;
 
 
 namespace Trestlebridge.Models.Facilities {
-    public class PlowField : IFacility<IGrazing>
+    public class PlowField : IFacility<IPlowing>
     {
-        private int _capacity = 50;
+        private int _capacity = 65;
         private Guid _id = Guid.NewGuid();
 
-        private List<IGrazing> _animals = new List<IGrazing>();
+        private List<IPlowing> _seeds = new List<IPlowing>();
 
         public double Capacity {
             get {
@@ -18,27 +18,28 @@ namespace Trestlebridge.Models.Facilities {
             }
         }
 
-        public void AddResource (IGrazing animal)
+        public void AddResource (IPlowing seed)
         {
-            _animals.Add(animal);
-            Console.WriteLine("Animal added to field");
+            _seeds.Add(seed);
+            Console.WriteLine("Seed added to field");
             Console.ReadLine();
         }
 
 
-        public void AddResource (List<IGrazing> animals)
+        public void AddResource (List<IPlowing> seeds)
         {
             // TODO: implement this...
             throw new NotImplementedException();
         }
+
 
         public override string ToString()
         {
             StringBuilder output = new StringBuilder();
             string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
 
-            output.Append($"Grazing field {shortId} has {this._animals.Count} animals\n");
-            this._animals.ForEach(a => output.Append($"   {a}\n"));
+            output.Append($"Plow field {shortId} has {this._seeds.Count} seeds\n");
+            this._seeds.ForEach(a => output.Append($"   {a}\n"));
 
             return output.ToString();
         }
